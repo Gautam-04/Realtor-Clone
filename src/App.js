e9855e2c7130ae5e0a24cddcfbb1b6f21aa7fc24
Gautam-666 <2022.gautam.rai@ves.ac.in>
2023-05-07 14:52:52 +0530

    Create Pages and Routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-
+import {BrowserRouter as Router , Routes, Route} from "react-router-dom";
+import Home from "./Pages/Home";
+import Offers from "./Pages/Offers";
+import ForgotPassword from "./Pages/ForgotPassword";
+import Profile from "./Pages/Profile";
+import SignIn from "./Pages/SignIn";
+import SignUp from "./Pages/SignUp";
 
 function App() {
   return (
     <>
-      <h1>Hello World!</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+      </Router>
     </>
   );
 }
