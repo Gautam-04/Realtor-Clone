74a2a3146e51c341019e7601a1197caac08f9c1d
Gautam-666 <2022.gautam.rai@ves.ac.in>
2023-07-09 19:52:41 +0530

    Added the functionality to the profile page
    Signed-off-by: Gautam-666 <2022.gautam.rai@ves.ac.in>

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,15 +1,46 @@
 import React, { useState } from 'react'
-import { getAuth } from 'firebase/auth';
+import { getAuth, updateProfile} from 'firebase/auth';
 import { useNavigate } from 'react-router';
+import { toast } from 'react-toastify';
+import { db } from "../firebase";
+import { doc, updateDoc } from "firebase/firestore";
 
 export default function Profile() {
   const auth = getAuth();
   const navigate = useNavigate();
+  const[changeDetail, setChangeDetail] = useState(false);
 
 const [formData, setformData] = useState({
   name: auth.currentUser.displayName,
   email: auth.currentUser.email
 });
+
+function onChange(e){
+setformData((prevState) => ({
+  ...prevState,
+  [e.target.id]: e.target.value,
+}))
+}
+
+async function onSubmit(){
+  try {
+    if(auth.currentUser.displayName != name){
+      //update the display name in firebase auth
+      await updateProfile(auth.currentUser,{
+        displayName: name,
+      });
+
+      //update the name in the firestore
+      const docRef = doc(db, "users", auth.currentUser.uid)
+      await updateDoc(docRef, {
+        name,
+      })
+    }
+    toast.success('Profile Details updated')
+  } catch (error) {
+    toast.error("Could Not update the profile details")
+  }
+}
 
 const { name, email } = formData;
 function onLogout(){
@@ -31,9 +62,11 @@ function onLogout(){
               type="text"
               id="name"
               value={name}
-              disabled
-              className="w-full px-4 py-2 text-xl text-gray-700 bg-white
-       border border-gray-300 rounded transition ease-in-out mb-6" 
+              disabled={!changeDetail}
+              onChange={onChange}
+              className={`w-full px-4 py-2 text-xl text-gray-700 bg-white
+       border border-gray-300 rounded transition ease-in-out mb-6 ${
+         changeDetail && "bg-red-200 focus:bg-red-200"}`}
             />
             <input
               type="email"
@@ -43,11 +76,25 @@ function onLogout(){
               className="w-full px-4 py-2 text-xl text-gray-700 bg-white
        border border-gray-300 rounded transition ease-in-out mb-6"
             />
-            <div className='flex justify-between whitespace-nowrap text-sm sm:text-lg mb-6'>
-            <p className='flex items-center '>Do You Want To Change Your Name??
-              <span className='text-red-600 hover:text-red-700 ease-in-out transition duration-200 ml-1 cursor-pointer'>Edit</span>
-            </p>
-            <p onClick={onLogout} className='text-blue-600 hover:text-blue-800 transition ease-in-out duration-200 cursor-pointer '>Sign Out</p>
+            <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg mb-6">
+              <p className="flex items-center ">
+                Do You Want To Change Your Name??
+                <span
+                  onClick={() => {
+                    changeDetail && onSubmit();
+                    setChangeDetail((prevState) => !prevState);
+                  }}
+                  className="text-red-600 hover:text-red-700 ease-in-out transition duration-200 ml-1 cursor-pointer"
+                >
+                  {changeDetail ? "Apply Change" : "Edit"}
+                </span>
+              </p>
+              <p
+                onClick={onLogout}
+                className="text-blue-600 hover:text-blue-800 transition ease-in-out duration-200 cursor-pointer "
+              >
+                Sign Out
+              </p>
             </div>
           </form>
         </div>
